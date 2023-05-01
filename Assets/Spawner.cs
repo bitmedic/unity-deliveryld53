4e65e33ff6f3f7ca71b04a5cd2342e97bc838be6
bitmedic <bitmedic01@gmail.com>
2023-05-01 09:34:18 +0200

    More guests over Time

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Guest spawnedObject;
-    public float spawnDelay;
+    public float spawnDelayInitial;
+    public float spawnDelayBetween;
+    public float secondsUntilMaxSpawnSpeed = 5 * 60;
+    public float maxSpawnSpeedMultiplier;
+    public float currentSpawnSpeedMultiplier;
+
+
+    private float startTime;
 
     void Start()
     {
-        InvokeRepeating(nameof(DoSpawn), 0f, spawnDelay);
+        StartCoroutine(DoSpawn());
+        currentSpawnSpeedMultiplier = 1;
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float elapsedTime = (Time.time - startTime);
+        float elapsedTimePercent = Mathf.InverseLerp(0, secondsUntilMaxSpawnSpeed, elapsedTime);
+        currentSpawnSpeedMultiplier = Mathf.Lerp(1, maxSpawnSpeedMultiplier, elapsedTimePercent);
     }
 
-    void DoSpawn()
+    IEnumerator DoSpawn()
     {
-        Guest guest = Instantiate(spawnedObject, transform.position, Quaternion.identity);
-        guest.FindPlace();
+        yield return new WaitForSeconds(spawnDelayInitial);
+        while (true)
+        {
+            Guest guest = Instantiate(spawnedObject, transform.position, Quaternion.identity);
+            guest.FindPlace();
+            float seconds = spawnDelayBetween / currentSpawnSpeedMultiplier;
+            Debug.Log("Next guest in " + seconds + " seconds.");
+            yield return new WaitForSeconds(seconds);
+        }
     }
 }
