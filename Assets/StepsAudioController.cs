7aa83752723ee6a3538bce5a36ccb48bf5d5b62e
noskro <matthiasluedemann@googlemail.com>
2023-05-01 14:40:58 +0200

    Character Movement Sounds und random Glass Sounds

---
+++ Assets/StepsAudioController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepsAudioController : MonoBehaviour
+{
+    public List<AudioClip> soundsSteps;
+    private AudioSource source;
+
+    public bool isWalking;
+    public float stepTimeInterval;
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWalking)
+        {
+            if (timer <= 0)
+            {
+                int randomStep = Random.Range(0, soundsSteps.Count);
+                source.PlayOneShot(soundsSteps[randomStep]);
+                timer = stepTimeInterval;
+            }
+
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+        }
+    }
+}
